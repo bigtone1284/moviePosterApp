7e410856bf31bb6ea51ca03340c8d945988aa9c0
Tony DeFreitas <anthony.defreitas@gmail.com>
2015-07-31 19:18:20 -0400

    working on paginator

--- public/js/controllers/home_controller.js
+++ public/js/controllers/home_controller.js
@@ -1,4 +1,25 @@
-movieApp.controller("homeController", function ($scope, movieFactory) {
+movieApp.controller("homeController", function ($scope, movieFactory, $routeParams) {
   $scope.headerSrc = "templates/header.html";
-  $scope.movies = movieFactory.get();
+  $routeParams.page = $routeParams.page || 1;
+  $scope.movies = movieFactory.get({page: $routeParams.page});
+  $scope.paginatorSrc = "templates/paginator.html";
+
+  $scope.ellipsesBefore = function () {
+		return ($scope.movies.total_pages > 3 && $routeParams.page > 2);
+  };
+
+  $scope.ellipsesAfter = function () {
+		return ($scope.movies.total_pages > 3 && $scope.movies.total_pages > $routeParams.page + 2);
+  };
+
+  $scope.posterLink = function (movie) {
+		if (movie.poster_path) {
+			return "http://image.tmdb.org/t/p/w342" + movie.poster_path;
+		} else if (movie.backdrop_path) {
+			return "http://image.tmdb.org/t/p/w342" + movie.backdrop_path;
+		} else {
+			return "http://cdn.shoptools.com/images/products/starrett/imagenotavailable.jpg";
+		}
+	};
+
 });
