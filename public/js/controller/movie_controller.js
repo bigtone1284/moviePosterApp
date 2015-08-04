7c2bac22d816c14c4bbde28c7de1362f985e06ba
Tony DeFreitas <anthony.defreitas@gmail.com>
2015-08-04 00:22:30 -0400

    nearly done, one more look at tomorrow and readme

--- public/js/controller/movie_controller.js
+++ public/js/controller/movie_controller.js
@@ -1,4 +1,4 @@
-movieApp.controller("movieController", function ($scope, $routeParams, $location, $rootScope, movies) {
+movieApp.controller("movieController", function ($scope, $routeParams, $location, $rootScope, $filter, movies) {
 	$rootScope.movies = movies;
 	$scope.getMovieById($routeParams.id);
 
@@ -10,4 +10,35 @@ movieApp.controller("movieController", function ($scope, $routeParams, $location
 			return "url(http://image.tmdb.org/t/p/w342" + $scope.currMovie.poster_path + ");";
 		}
 	};
+
+	$scope.lastMovie = function (movieId) {
+		return movieId % 20 === 0;
+	};
+
+	$scope.movieTitle = function () {
+		var releaseDate = new Date($scope.currMovie.release_date);
+		return $scope.currMovie.title + " (" + $filter('date')(releaseDate, "yyyy") + ")";
+	};
+
+	$scope.details = function () {
+		var score = "Score: " + $scope.currMovie.vote_average;
+		var releaseDate = new Date($scope.currMovie.release_date);
+		var release = "Release Date: " + $filter('date')(releaseDate, "longDate");
+		return [score, release].join(' | ');
+	};
+
+	$scope.vertDetails = function () {
+		var score = $scope.currMovie.vote_average;
+		var releaseDate = new Date($scope.currMovie.release_date);
+		var release = $filter('date')(releaseDate, "longDate");
+		return {
+			score: score,
+			releaseDate: release
+		};
+	};
+
+	$scope.overview = function () {
+		return $scope.currMovie.overview;
+	};
+
 });
